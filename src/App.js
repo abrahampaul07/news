bb88e7a67ed189b31970ba0b738a133957321ea8
abrahampaul07 <abrahamise.rymec@gmail.com>
2021-07-26 03:31:01 +0530

    news

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import NavInshorts from "./components/NavInshorts";
+import NewsContent from "./components/NewsContent/NewsContent";
+import apikey from "./data/config";
+
+function App() {
+    const [category, setCategory] = useState("general");
+    const [newsArray, setNewsArray] = useState([]);
+    const [newsResults, setNewsResults] = useState();
+    const [loadmore, setLoadmore] = useState(20);
+
+
+    const newsApi=async() => {
+        try{
+           
+            const news = await axios.get(
+                `https://newsapi.org/v2/top-headlines?country=in&apiKey=${apikey}&category=${category}&pageSize=${loadmore}`
+            );
+            setNewsArray(news.data.articles);
+            setNewsResults(news.data.totalResults);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    console.log(newsArray);
+
+    useEffect(() => {
+        newsApi();
+    }, [newsResults,category,loadmore]);
+
+    return <div className="App">
+    <NavInshorts setCategory={setCategory} />
+
+    <NewsContent setLoadmore={setLoadmore} loadmore={loadmore} newsArray={newsArray} newsResults={newsResults} />
+    <Footer />
+    </div>
+}
+
+export default App;
